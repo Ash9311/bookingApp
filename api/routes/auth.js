53f3c424ebd90d5e5217a772ce403e3148700354
@Ash9311 <ashutoshmulki7@gmail.com>
2023-06-25 19:24:33 +0530

    login/register

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,9 +1,8 @@
 import express from "express"
-
+import { login,register } from "../controllers/auth.js";
 const router = express.Router();
 
-router.get("/",(req,res)=>{
-    res.send("this is auth end point")
-})
+router.post("/register",register)
+router.post("/login",login)
 
 export default router;
